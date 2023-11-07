8cee2a7f38914aa619dd250d79f976b6032a8529
Shaham Noorani <shahamnoorani@gmail.com>
2023-11-06 23:46:21 -0600

    adding chat_gpt_reccomender

--- chat_gpt_reccomender.py
+++ chat_gpt_reccomender.py
@@ -9,38 +9,52 @@ load_dotenv()
 
 def chatgpt_reccomendation(reccomendation_set):
     # Load the top 500 anime from the CSV file
-    top_anime = []
+    top_anime_names_genres = []
+    top_anime_scores = {}
     with open("data/top_500_anime.csv", "r") as f:
         reader = csv.reader(f)
+        reader.__next__()
         for row in reader:
-            top_anime.append(row[0])
+            top_anime_names_genres.append(f"{row[0]} - {row[2]}/{row[3]}")
+            top_anime_scores[row[0]] = row[1]
 
     # Use the Chat GPT API to generate recommendations based on the input anime
     openai.api_key = os.getenv("OPENAI_API_KEY")
-    prompt = f"Recommend me some anime similar to {', '.join(reccomendation_set)}"
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        max_tokens=60,
-        n=10,
-        stop=None,
-        temperature=0.5,
+    messages = [
+        "I'm going to give you a list of the top anime on MyAnimeList right now.",
+        top_anime_names_genres.__str__(),
+        f'Please reccomend me up to 15 anime (just the names) that are very similar to {", ".join(reccomendation_set)}.',
+        "Please only reccomend anime from list of top anime I provided and format the output as a comma seperated list.",
+    ]
+
+    prompt = []
+    for message in messages:
+        prompt.append({"role": "user", "content": message})
+
+    response = openai.ChatCompletion.create(
+        model="gpt-4-1106-preview",
+        messages=prompt,
+        temperature=0,
+        max_tokens=500,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
     )
-    recommendations = [choice.text.strip() for choice in response.choices]
 
-    # Filter the top 500 anime to remove any that are already in the input list
-    remaining_anime = [anime for anime in top_anime if anime not in reccomendation_set]
+    # parse the response and trim whitespace
+    recommendations = response.choices[0].message.content.split(",")
+    recommendations = [r.strip() for r in recommendations]
 
-    # Sort the remaining anime by their recommendation score
-    scored_anime = []
-    for anime in remaining_anime:
-        score = 0
-        for recommendation in recommendations:
-            if anime in recommendation:
-                score += 1
-        scored_anime.append((anime, score))
-    scored_anime.sort(key=lambda x: x[1], reverse=True)
+    # sort recomedations by score and get the top 5
+    recommendations.sort(key=lambda x: top_anime_scores[x], reverse=True)
+    print(recommendations[:5])
 
-    # Return the top 10 anime as recommendations
-    top_recommendations = [anime[0] for anime in scored_anime[:10]]
-    print(top_recommendations)
+    return recommendations[:5]
+
+
+chatgpt_reccomendation(
+    [
+        "Naruto - Action/Adventure",
+        "One Punch Man - Action/Comedy",
+    ]
+)
