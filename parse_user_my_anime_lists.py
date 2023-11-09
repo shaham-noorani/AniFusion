1f94365d9a423051301ab3fc88a019fa80080cf9
Shaham Noorani <shahamnoorani@gmail.com>
2023-11-09 15:50:29 -0600

    pushing format for user shows object

--- parse_user_my_anime_lists.py
+++ parse_user_my_anime_lists.py
@@ -8,8 +8,22 @@ print(os.getenv("MAL_CLIENT_ID"))
 
 import requests
 
+# get the raw data from the xmls
+user_shows = [
+    {
+        animedb_id: 12124321,
+        title: "sd",
+        series_type: "tv",
+        my_score: 8,
+        status: "completed",
+    }
+]
+
+# augment with api data
 url = "https://api.myanimelist.net/v2/anime/52034?fields=genres"
 payload = {}
 headers = {"X-MAL-CLIENT-ID": os.getenv("MAL_CLIENT_ID")}
 response = requests.request("GET", url, headers=headers, data=payload)
 print(response.text)
+
+# return that data
