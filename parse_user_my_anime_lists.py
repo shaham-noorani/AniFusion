7095ecfc6e174dfa9c0969b5abdab3a3073914c5
Shaham Noorani <shahamnoorani@gmail.com>
2023-11-09 15:42:06 -0600

    added code for making api request

--- parse_user_my_anime_lists.py
+++ parse_user_my_anime_lists.py
@@ -5,3 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 print(os.getenv("MAL_CLIENT_ID"))
+
+import requests
+
+url = "https://api.myanimelist.net/v2/anime/52034?fields=genres"
+payload = {}
+headers = {"X-MAL-CLIENT-ID": os.getenv("MAL_CLIENT_ID")}
+response = requests.request("GET", url, headers=headers, data=payload)
+print(response.text)
